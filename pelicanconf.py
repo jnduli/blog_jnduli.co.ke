1b77fd7e4af4a78afbef80151403eda3d00c191c
John Nduli <yohanaizraeli@gmail.com>
2019-04-18 12:17:01 +0300

    Set default article status to draft

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,6 +31,10 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
